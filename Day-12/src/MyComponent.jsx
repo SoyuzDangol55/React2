d8242b7e4b5990139dc1ffc0f69d10caeb582b4b
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-18 23:40:07 +0545

    style:Add Style to MyComponent

--- Day-12/src/MyComponent.jsx
+++ Day-12/src/MyComponent.jsx
@@ -32,16 +32,16 @@ function MyComponent(){
     }
 return(
     <>
-    <div>
+    <div className="main-div">
         <h2>List of Car Objects</h2>
         <ul>
         {cars.map((car,index) => 
             <li key={index} onClick={() => handleRemoveCar(index)}>{car.year} {car.make} {car.model}</li>
         )}
         </ul>
-        <input type="number" value={carYear} onChange={handleYearChange} /><br />
-        <input type="text" value={carMake} onChange={handleMakeChange}/><br />
-        <input type="text" value={carModel} onChange={handleModelChange}/><br />
+        <input type="number" value={carYear} onChange={handleYearChange} placeholder="Enter Year of the car"/><br />
+        <input type="text" value={carMake} onChange={handleMakeChange}placeholder="Enter Car Make"/><br />
+        <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car Model"/><br />
         <button onClick={handleAddCar}>Add Car</button>
     </div>
     </>
